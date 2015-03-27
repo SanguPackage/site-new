c25530434ecefa612f94502a25d062b18f4230a3
Laoujin <woutervs@hotmail.com>
2015-03-27 01:48:48 +0100

    popups/tooltips

---
+++ site/js/index/sp-carousel.js
@@ -0,0 +1,68 @@
+(function($) {
+	var spCarousel = $('#sp-jcarousel');
+
+	// Sangu Package Carousel
+	// Docs: http://sorgalla.com/jcarousel/docs/
+	function startCarousel() {
+		spCarousel
+			.jcarousel({
+				wrap: 'circular'
+			})
+			.jcarouselAutoscroll({
+				interval: 3000
+			});
+	}
+
+	function stopCarousel() {
+		spCarousel.jcarouselAutoscroll('stop');
+		spCarousel.jcarousel('reload', {
+			wrap: null
+		});
+	}
+
+	function restartCarousel() {
+		spCarousel.jcarouselAutoscroll('start');
+		spCarousel.jcarousel('reload', {
+			wrap: 'circular'
+		});
+	}
+
+	function addNavigationButtonBehavior(buttonSelector, slideDirection) {
+		$(buttonSelector)
+			.on('click', stopCarousel)
+			.on('jcarouselcontrol:active', function() {
+				$(this).removeClass('jcarousel-inactive');
+			})
+			.on('jcarouselcontrol:inactive', function() {
+				$(this).addClass('jcarousel-inactive');
+			})
+			.jcarouselControl({
+				target: slideDirection
+			});
+	}
+
+	startCarousel();
+	addNavigationButtonBehavior('.jcarousel-control-prev', '-=1');
+	addNavigationButtonBehavior('.jcarousel-control-next', '+=1');
+
+	// Sangu Package Carousel popup
+	$('a.popup-thumbnail').fullScreenPopup({
+		bgColor: '#c8aa6e',
+		//inlineStyles: true,
+		//mainWrapperClass: 'twbackground',
+		//contentWrapperClass: 'twbackground',
+		closePopupClass: 'fix-bootstrap-link sp-jcarousel-popup-close',
+		animationSpeed: 300, //ms
+		updatePopup: function(popup, link) {
+			// Set correct image in popup
+			var imgUrl = $(link).attr('data-img');
+			var popupImg = $('img', popup).first();
+			popupImg.attr('src', imgUrl);
+
+			stopCarousel();
+		},
+		close: function() {
+			restartCarousel();
+		}
+	});
+})(jQuery);
--- site/js/main.js
+++ site/js/main.js
@@ -1,68 +1,19 @@
 (function($) {
-	var spCarousel = $('#sp-jcarousel');
+	//new Opentip("#my-element", "Shown after 2 seconds", { delay: 0.5 });
+	//$("#my-element").opentip("Shown after 2 seconds", { delay: 2 });
 
-	// Sangu Package Carousel
-	// Docs: http://sorgalla.com/jcarousel/docs/
-	function startCarousel() {
-		spCarousel
-			.jcarousel({
-				wrap: 'circular'
-			})
-			.jcarouselAutoscroll({
-				interval: 3000
-			});
-	}
+	Opentip.styles.mystyle = {
+	  // Make it look like the alert style. If you omit this, it will default to "standard"
+	  extends: "dark",
+	  // Tells the tooltip to be fixed and be attached to the trigger, which is the default target
+	  target: true,
+	  //stem: true,
+	  //showOn: "creation"
+	  tipJoint: "bottom"
+	};
 
-	function stopCarousel() {
-		spCarousel.jcarouselAutoscroll('stop');
-		spCarousel.jcarousel('reload', {
-			wrap: null
-		});
-	}
+	Opentip.defaultStyle = "mystyle";
 
-	function restartCarousel() {
-		spCarousel.jcarouselAutoscroll('start');
-		spCarousel.jcarousel('reload', {
-			wrap: 'circular'
-		});
-	}
 
-	function addNavigationButtonBehavior(buttonSelector, slideDirection) {
-		$(buttonSelector)
-			.on('click', stopCarousel)
-			.on('jcarouselcontrol:active', function() {
-				$(this).removeClass('jcarousel-inactive');
-			})
-			.on('jcarouselcontrol:inactive', function() {
-				$(this).addClass('jcarousel-inactive');
-			})
-			.jcarouselControl({
-				target: slideDirection
-			});
-	}
 
-	startCarousel();
-	addNavigationButtonBehavior('.jcarousel-control-prev', '-=1');
-	addNavigationButtonBehavior('.jcarousel-control-next', '+=1');
-
-	// Sangu Package Carousel popup
-	$('a.popup-thumbnail').fullScreenPopup({
-		bgColor: '#c8aa6e',
-		//inlineStyles: true,
-		//mainWrapperClass: 'twbackground',
-		//contentWrapperClass: 'twbackground',
-		closePopupClass: 'fix-bootstrap-link sp-jcarousel-popup-close',
-		animationSpeed: 300, //ms
-		updatePopup: function(popup, link) {
-			// Set correct image in popup
-			var imgUrl = $(link).attr('data-img');
-			var popupImg = $('img', popup).first();
-			popupImg.attr('src', imgUrl);
-
-			stopCarousel();
-		},
-		close: function() {
-			restartCarousel();
-		}
-	});
 })(jQuery);
