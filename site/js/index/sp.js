1ebfeee727286afdf97fc8650f588cf9ed882368
Laoujin <woutervs@hotmail.com>
2015-03-27 04:07:41 +0100

    some refactoring, ie bugfix and notice sp is nl only

---
+++ site/js/index/sp.js
@@ -0,0 +1,23 @@
+(function($) {
+	var userLanguage = window.navigator.userLanguage || window.navigator.language;
+	if (userLanguage.indexOf('nl') === -1) {
+		$('#sp-dutch-only').show();
+	}
+
+
+
+	// Sangu Package Installation
+	var spInstallTip = new Opentip($('#sp-install'));
+	spInstallTip.setContent('Installatie instructies voor jouw browser');
+
+	$('#sp-install').click(function() {
+		// Stop showing the tooltip after the installation instructions have been visible
+		spInstallTip.deactivate();
+		spInstallTip.hide();
+
+		// Show/Hide the installation instructions
+		var popup = $('#sp-installation-instructions')
+		popup.slideToggle();
+	});
+
+})(jQuery);
--- site/js/main.js
+++ site/js/main.js
@@ -14,23 +14,4 @@
 
 	Opentip.defaultStyle = 'mystyle';
 
-
-
-
-
-
-	// Sangu Package Installation
-	var spInstallTip = new Opentip($('#sp-install'));
-	spInstallTip.setContent('Installatie instructies voor jouw browser');
-
-	$('#sp-install').click(function() {
-		// Stop showing the tooltip after the installation instructions have been visible
-		spInstallTip.deactivate();
-		spInstallTip.hide();
-
-		// Show/Hide the installation instructions
-		var popup = $('#sp-installation-instructions')
-		popup.slideToggle();
-	});
-
 })(jQuery);
