42c4e28401db258e080d8fe6b434055f0bea1252
Laoujin <woutervs@hotmail.com>
2015-05-25 13:50:42 +0200

    fixed -redirect issue

--- site/api/twtactics-send-ga.php
+++ site/api/twtactics-send-ga.php
@@ -1,6 +1,4 @@
 <?php
-//header('Content-type: application/javascript');
-
 include "ga/autoload.php";
 
 use UnitedPrototype\GoogleAnalytics;
@@ -15,14 +13,9 @@ $visitor->setIpAddress($_SERVER['REMOTE_ADDR']);
 $visitor->setUserAgent($_SERVER['HTTP_USER_AGENT']);
 $visitor->setScreenResolution('1024x768');
 
-$event = new GoogleAnalytics\Event('Downloads', 'TWTactics-' . $_GET['type'], '1.0');
+$event = new GoogleAnalytics\Event('Downloads', 'TWTactics-exe', '1.0');
 
 $tracker->trackEvent($event, $session, $visitor);
 
-switch ($_GET['type'])
-{
-	case 'zip':
-		header('Location: twtactics-redirect.zip');
-		break;
-}
-?>
+header('Content-disposition: attachment; filename="TWTactics.exe"');
+readfile("TWTactics-redirect.exe");?>
