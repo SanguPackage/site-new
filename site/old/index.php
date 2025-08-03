7359f8a5a4dcf907c27bb875b06bfc3624204b25
Wouter Van Schandevijl <woutervs@hotmail.com>
2025-08-03 23:04:28 +0200

    ga

--- site/old/index.php
+++ site/old/index.php
@@ -880,14 +880,12 @@ op 07.02. om 13:31 uur
 -->
 </td></tr></table>
 
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-30075487-3"></script>
 <script>
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-30075487-3', 'auto');
-ga('send', 'pageview');
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-30075487-3');
 </script>
 
 </body>
